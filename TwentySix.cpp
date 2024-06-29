025cb2a676ea1d17ed611c11b738b2a74d9be719
Neeraj Kumar <neerajkumar42p@gmail.com>
2024-06-29 23:48:53 +0530

    Commit

---
+++ TwentySix.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string a;
+    string b;
+    cin>>a>>b;
+    cout<<"The strings which you have given as input are:"<<a<<b;
+    string c;
+    c=a+b;
+    cout<<"The combined word is:"<<c;
+    return 0;}
